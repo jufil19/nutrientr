13478853f5ac7750d9d4896819901015afae2fea
SaraAnnHall <sara.hall@shaw.ca>
2022-02-08 16:40:06 -0800

    Add unit tests for cn_api and fix some error handling.

---
+++ tests/testthat/test-cn_api.R
@@ -0,0 +1,49 @@
+test_that("food endpoint is reachable", {
+  resp <- cn_api('food', '664fbadd34e6c2ca75235709f9ede2bd', id = 1)
+  expect_equal(resp$response$status_code, 200)
+})
+
+test_that("nutrientamount endpoint is reachable", {
+  resp <- cn_api('nutrientamount', '664fbadd34e6c2ca75235709f9ede2bd', id=1)
+  expect_equal(resp$response$status_code, 200)
+})
+
+test_that("nutrientgroup is reachable", {
+  resp <- cn_api('nutrientgroup', '664fbadd34e6c2ca75235709f9ede2bd', id = 1)
+  expect_equal(resp$response$status_code, 200)
+})
+
+test_that("nutrientname endpoint is reachable", {
+  resp <- cn_api('nutrientname', '664fbadd34e6c2ca75235709f9ede2bd')
+  expect_equal(resp$response$status_code, 200)
+})
+
+test_that("nutrientsource endpoint is reachable", {
+  resp <- cn_api('nutrientsource', '664fbadd34e6c2ca75235709f9ede2bd')
+  expect_equal(resp$response$status_code, 200)
+})
+
+test_that("refuseamount endpoint is reachable", {
+  resp <- cn_api('refuseamount', '664fbadd34e6c2ca75235709f9ede2bd')
+  expect_equal(resp$response$status_code, 200)
+})
+
+test_that("servingsize endpoint is reachable", {
+  resp <- cn_api('servingsize', '664fbadd34e6c2ca75235709f9ede2bd')
+  expect_equal(resp$response$status_code, 200)
+})
+
+test_that("yieldamount endpoint is reachable", {
+  resp <- cn_api('yieldamount', '664fbadd34e6c2ca75235709f9ede2bd')
+  expect_equal(resp$response$status_code, 200)
+})
+
+test_that("Invalid endpoint will yield an error", {
+  resp <- cn_api('yieldamout', '664fbadd34e6c2ca75235709f9ede2bd')
+  expect_equal(resp$response$status_code, 200)
+})
+
+testthat::test_that("When incorrect key to the API is given, error is thrown", {
+  incorrect_key <- "incorrect"
+  expect_error(cn_api(path = "food", apiKey = incorrect_key, id = 2314))
+})
