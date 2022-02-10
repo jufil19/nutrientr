bf5740d5fb8a6e7ac239f2deab3c6e2252bcb592
Justine Filion <filionjustine05@gmail.com>
2022-02-10 14:37:20 -0800

    Combined seperate utility functions into one file

--- R/utility/utility.r
+++ R/utility/utility.r
@@ -1,3 +1,37 @@
+#' Retrieves the API key
+#'
+#' @description
+#' `get_key` stops execution and returns an error message if the environment variable `CN_API` is not set to the user's API key. If the environment
+#' variable is not set, the function returns an error message.
+#'
+#' @return The user's API key, if set as an environment variable. If key is not set, returns an error message indicating that the user can either set
+#' the environment variable `CN_API` or pass the API key as an argument to the function.
+
+#' @references Note that this function is based on, and repeats much of the code in
+#'   the R API package best practices, available at <https://httr.r-lib.org/articles/api-packages.html#overall-design-1>.
+#' @examples
+#' get_key()
+get_key <- function() {
+  key <- Sys.getenv("CN_API")
+  if (key == "") {
+    stop("An API key is required. Please set environment variable `CN_API` to your API key as so : `Sys.setenv(CN_API = 'your_key')` or pass
+          an API key as an argument to the function. An API key can be acquired at <https://hc-sc.api.canada.ca/en/detail?api=cnf>", call. = FALSE)
+  }
+
+  key
+}
+
+
+# helper function that skips unit tests if no API key is set
+# This function will not be visible by users (inspired by: https://cran.r-project.org/web/packages/httr/vignettes/secrets.html)
+
+skip_if_no_key <- function() {
+  if (Sys.getenv("CN_API") == "") {
+    skip("No API key available")
+  }
+}
+
+
 #' query_api
 #' Makes a query to the Canadian Nutrient Files API.
 #'
@@ -83,3 +117,26 @@ query_api <- function(path, apiKey = NULL, id = NULL, lang = "en") {
     class = "cn_api"
   )
 }
+
+
+# Helper function that formats the output of the query_api function into a tibble with appropriate column types
+# This function will not be visible by users
+
+clean_data <- function(query_api_output) {
+  df <- as.data.frame(do.call(rbind, query_api_output$content))
+
+  # Check if the dataframe is empty
+
+  if (nrow(df) == 0) {
+    stop("The requested query has no result")
+  }
+
+  # Each column is a list, we thus need to unnest every column
+  # This way, R will be able to attribute the appropriate type to each column
+
+  df_unnest <- unnest(df, cols = all_of(colnames(df)))
+
+  # output the tibble
+  df_unnest
+}
+
